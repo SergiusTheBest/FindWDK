74a503dfb58046ad63d8728f5fb1d24ad6ef7817
John Tyner <jtyner@fhoosh.com>
2018-03-26 07:29:49 -0700

    don't check for existence of latest ddk file until after find_package_handle_standard_args does its thing

--- cmake/FindWdk.cmake
+++ cmake/FindWdk.cmake
@@ -35,14 +35,16 @@ file(GLOB WDK_NTDDK_FILES
     "C:/Program Files*/Windows Kits/10/Include/*/km/ntddk.h"
 )
 
-if(NOT WDK_NTDDK_FILES)
-    return()
+if(WDK_NTDDK_FILES)
+    list(GET WDK_NTDDK_FILES -1 WDK_LATEST_NTDDK_FILE)
 endif()
-
-list(GET WDK_NTDDK_FILES -1 WDK_LATEST_NTDDK_FILE)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WDK REQUIRED_VARS WDK_LATEST_NTDDK_FILE)
+
+if (NOT WDK_LATEST_NTDDK_FILE)
+    return()
+endif()
 
 get_filename_component(WDK_ROOT ${WDK_LATEST_NTDDK_FILE} DIRECTORY)
 get_filename_component(WDK_ROOT ${WDK_ROOT} DIRECTORY)
